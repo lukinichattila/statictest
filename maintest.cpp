54cf090d32080c175c3cf194685f12b2568c0930
Lukinich Attila <dstudiopro@gmail.com>
2022-06-22 23:12:01 +0300

    static test confirmed

--- maintest.cpp
+++ maintest.cpp
@@ -2,11 +2,17 @@
 #include "operation.h"
 #include<gtest/gtest.h>
 #include<gmock/gmock.h> 
-
+extern double pi;
 TEST_F(TestOperation,intsummatest){
-   EXPECT_CALL(*mockadditionpointer, intsumma(_,_)).WillOnce(Return(3)); 
+   EXPECT_CALL(*mockadditionpointer, intsumma(1,2)).WillOnce(Return(3)); 
     int result=intoperation (1,2);
    EXPECT_EQ(3,result);
+}
+TEST_F(TestOperation,pioperation){
+    pi=1;
+   EXPECT_CALL(*mockadditionpointer, floatsumma(_,_)).WillOnce(Return(2)); 
+    double result=pioperation (2);
+   EXPECT_EQ(5.14,result);
 }
 int main(int argc, char **argv){
     testing::InitGoogleTest(&argc, argv);
--- src/operation.cpp
+++ src/operation.cpp
@@ -7,5 +7,5 @@ double doubleoperation(double a,double b){
     return(floatsumma(a,b));
 };
 double pioperation(double a){
-    return (pi+a);
+    return (pi+floatsumma(0,a));
 }
